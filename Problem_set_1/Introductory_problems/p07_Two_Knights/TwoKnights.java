39588e437fb618fe943199aa0a19c4f76681e515
SimpleSlick <b40881067@gmail.com>
2026-06-10 11:59:38 +0530

    Solved Two Knights Problem

---
+++ Problem_set_1/Introductory_problems/p07_Two_Knights/TwoKnights.java
@@ -0,0 +1,20 @@
+package p07_Two_Knights;
+
+import java.util.Scanner;
+
+public class TwoKnights {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    
+        long n = scan.nextLong();
+
+        for(long j = 1; j <= n; j++){
+            long totalWays = j * j * (j * j - 1) / 2;
+            long attackingWays = 4 * (j - 1) * (j - 2);
+
+            long result = totalWays - attackingWays;
+            System.out.println(result);
+        }
+        scan.close();
+    }
+}
